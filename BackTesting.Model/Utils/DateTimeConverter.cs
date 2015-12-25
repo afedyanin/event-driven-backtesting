b211b2249b65ea3735a64b1c41bdfccd955cf0e1
afedyanin <anfemail@gmail.com>
2015-12-25 16:36:39 +0300

    Adding DateTime converter

---
+++ BackTesting.Model/Utils/DateTimeConverter.cs
@@ -0,0 +1,41 @@
+﻿namespace BackTesting.Model.Utils
+{
+    using System;
+    using System.Globalization;
+
+    public static class DateTimeStringConverter
+    {
+        private const string CONST_DateFormat = "yyyyMMdd";
+        private const string CONST_DateTimeFormat = "yyyyMMdd HHmmss";
+
+        public static DateTime? Convert(string date, string time = null)
+        {
+            if (string.IsNullOrEmpty(date))
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(time))
+            {
+                return ConvertDate(date);
+            }
+
+            return ConvertDateTime(date, time);
+        }
+
+        private static DateTime? ConvertDate(string date)
+        {
+            DateTime res;
+            var isConverted = DateTime.TryParseExact(date, CONST_DateFormat, null, DateTimeStyles.None, out res);
+            return isConverted ? (DateTime?)res : null;
+        }
+
+        private static DateTime? ConvertDateTime(string date, string time)
+        {
+            var dateTime = $"{date.Trim()} {time.Trim()}";
+            DateTime res;
+            var isConverted = DateTime.TryParseExact(dateTime, CONST_DateTimeFormat, null, DateTimeStyles.None, out res);
+            return isConverted ? (DateTime?)res : null;
+        }
+    }
+}
---
+++ BackTesting.Tests/DateTimeStringConverterTests.cs
@@ -0,0 +1,35 @@
+﻿namespace BackTesting.Tests
+{
+    using System;
+    using Model.Utils;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DateTimeStringConverterTests
+    {
+        [Test]
+        public void ConvertEmptyDateStringReturnsNull()
+        {
+            var res = DateTimeStringConverter.Convert(string.Empty);
+            Assert.IsNull(res);
+        }
+
+        [Test]
+        public void ConvertValidDateStringReturnsValidDate()
+        {
+            var res = DateTimeStringConverter.Convert("20151123");
+            var expectedDate = new DateTime(2015, 11, 23);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(expectedDate, res);
+        }
+
+        [Test]
+        public void ConvertValidDateTimeStringsReturnsValidDateTime()
+        {
+            var res = DateTimeStringConverter.Convert("20151123", "100226");
+            var expectedDate = new DateTime(2015, 11, 23, 10, 02, 26);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(expectedDate, res);
+        }
+    }
+}
